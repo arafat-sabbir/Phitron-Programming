a7edceb06cde31b20f073dde370e70967e232b2d
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-19 08:22:45 +0600

    Explored O_N Time Complexity In DSA

--- Basic_Data_Structure/O_N.cpp
+++ Basic_Data_Structure/O_N.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-  cout<<"Cpp Is Running"<<endl;
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i =0;i<n;i++){
+    cin>>arr[i];
+  }
+  int sum =0;
+  for(int i = 0;i<n;i++){
+    sum+=arr[i];
+  }
+  cout<<sum;
   return 0;
 }

613e4fe4c63699c4daa37712b4381436da5c1bc3
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-20 18:50:23 +0600

    Explored N*N Time complexity In Dsa using Cpp

--- Basic_Data_Structure/O_sqrtN.cpp
+++ Basic_Data_Structure/O_sqrtN.cpp
@@ -5,7 +5,16 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 1; i <= sqrt(n); i++)
+    // for (int i = 1; i <= sqrt(n); i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << i <<  ' ';
+    //         if (n / i != i)
+    //             cout << n / i << endl;
+    //     }
+    // }
+    for (int i = 1; i*i <= n; i++)
     {
         if (n % i == 0)
         {

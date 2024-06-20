1ef5429256222f8f932e8b416bc332f19eae7780
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-20 19:33:46 +0600

    Explored O of N Log N In Dsa Using Cpp

---
+++ Basic_Data_Structure/O_N_Log_N.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i = 1;i<=n;i++){
+    int x = i;
+    while(x>0){
+        int digit = x%10;
+        cout<<digit<<" "<<endl;
+        x/=10;
+    }
+  }
+  return 0;
+}

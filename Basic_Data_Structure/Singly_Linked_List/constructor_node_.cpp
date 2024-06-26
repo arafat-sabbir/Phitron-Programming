88c60db4c7a470741c1db0edc860575433533e07
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-26 09:52:59 +0600

    Added Constructor Function on The Node Class Of Singly Linked List

--- Basic_Data_Structure/Singly_Linked_List/constructor_node_.cpp
+++ Basic_Data_Structure/Singly_Linked_List/constructor_node_.cpp
@@ -3,15 +3,16 @@ using namespace std;
 class Node{
     public:
         int val;
-        Node* next;        
+        Node* next;   
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }     
 };
 int main()
 {
-  Node a,b;
-  a.val = 10;
-  b.val = 20;
-  a.next = &b;
-  b.next = NULL;
+  Node a(10),b(20);
+    a.next = &b;
   cout<<a.val<<endl;
   cout<<b.val<<endl;
   cout<<a.next->val<<endl;
